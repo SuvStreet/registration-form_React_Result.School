dd25a95a11a1995259bac8e3d4b77d6537c5110b
Dmitrij Kolotusha <suvstreet@gmail.com>
2024-03-04 22:18:34 +0300

    delete comment

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -51,71 +51,9 @@ function Form() {
 				</div>
 
 				<form className={s.form} onSubmit={handleSubmit(onSubmit)} noValidate>
+
 					<Input {...{ register, errorsForm, watch, setFocus }} />
-
-					{/* <div className={s.input_content}>
-						<input
-							type='email'
-							name='email'
-							className={
-								s.input +
-								' ' +
-								(watch('email').length !== 0 ? s.isEmpty : '') +
-								' ' +
-								(errorsForm.email !== undefined ? s.error : '')
-							}
-							autoComplete='off'
-							{...register('email')}
-						/>
-						<label className={s.label}>Электронная почта</label>
-						{errorsForm.email && <div className={s.loginError}>{errorsForm.email}</div>}
-					</div>
-
-					<div className={s.input_content}>
-						<input
-							type='password'
-							name='password'
-							className={
-								s.input +
-								' ' +
-								(watch('password').length !== 0 ? s.isEmpty : '') +
-								' ' +
-								(errorsForm.password !== undefined ? s.error : '')
-							}
-							autoComplete='off'
-							{...register('password')}
-						/>
-						<label className={s.label}>Пороль</label>
-						{errorsForm.password && (
-							<div className={s.loginError}>{errorsForm.password}</div>
-						)}
-					</div>
-
-					<div className={s.input_content}>
-						<input
-							type='password'
-							name='confirmPassword'
-							className={
-								s.input +
-								' ' +
-								(watch('confirmPassword').length !== 0 ? s.isEmpty : '') +
-								' ' +
-								(errorsForm.confirmPassword !== undefined ? s.error : '')
-							}
-							autoComplete='off'
-							{...register('confirmPassword')}
-							{...(errorsForm.confirmPassword === undefined &&
-							watch('confirmPassword') === watch('password') &&
-							watch('password').length !== 0
-								? setFocus('submit')
-								: '')}
-						/>
-						<label className={s.label}>Повторите пароль</label>
-						{errorsForm.confirmPassword && (
-							<div className={s.loginError}>{errorsForm.confirmPassword}</div>
-						)}
-					</div> */}
-
+					
 					<button className={s.button} {...register('submit')} type='submit'>
 						Зарегистрироваться
 					</button>
